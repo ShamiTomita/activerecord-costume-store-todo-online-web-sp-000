ed08901ed8fe9042f87141d5a46ba4309821919f
Shami Tomita <shamitomita@gmail.com>
2021-04-20 17:48:43 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :location
       t.integer :costume_inventory
       t.integer :employees
-      t.string :status 
+      t.boolean :still_in_business
       t.datetime :opening_time 
       t.datetime :closing_time
     end 
