3a021bd8b212dc068543c249f4b775cd2285f0fa
Shami Tomita <shamitomita@gmail.com>
2021-04-20 17:43:57 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,10 +2,12 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
   
   def change 
     create_table :costume_stores do |t|
-      t.string :name, 
-      t.string :location,
-      t.integer :costume_inventory, 
-      t.integer :employees,
-      t.timestamps
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.string :status 
+      t.datetime :opening_time 
+      t.datetime :closing_time
     end 
   end
