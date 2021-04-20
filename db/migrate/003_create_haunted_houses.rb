1eaf8dabf4b7c76a957f551496e28be2a217660b
Shami Tomita <shamitomita@gmail.com>
2021-04-20 17:50:26 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,9 +2,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   
   def change 
     create_table :haunted_houses do |t|
-      t.string :name, 
-      t.string :location,
-      t.string :theme, 
+      t.string :name 
+      t.string :location
+      t.string :theme
       t.float :price
       t.boolean :family_friendly
       t.datetime :opening_date
