2bb84857e3062484ad2b8ac5ec02f80e70dec951
Shami Tomita <shamitomita@gmail.com>
2021-04-20 17:41:03 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,4 +13,9 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change 
     create_table :costumes do |t|
       t.string :name, 
+      t.string :price,
+      t.string :size, 
+      t.string :image_url
+      t.timestamps
+    end 
   end 
